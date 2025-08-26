8675eb496b56eabdb72c5915965c7f82c7161a4d
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2025-08-26 15:23:46 +0300

    chore: Update ESLint configuration and add new linting scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,30 @@
+import perfectionist from 'eslint-plugin-perfectionist';
+
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
-  // Your custom configs here
-)
+  [
+    {
+      rules: {
+        'no-console': [
+          'error',
+          {
+            allow: [
+              'info',
+              'warn',
+              'trace',
+              'error',
+              'group',
+              'groupEnd',
+              'time',
+              'timeEnd',
+            ],
+          },
+        ],
+      },
+    },
+    // @link https://perfectionist.dev/configs/recommended-alphabetical
+    perfectionist.configs['recommended-alphabetical'],
+  ],
+);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+/* eslint-disable nuxt/nuxt-config-keys-order */
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
   devtools: { enabled: true },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'always-multiline',
+        indent: 2,
+        quotes: 'single',
+        semi: true,
+      },
+    },
+  },
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/test-utils'
-  ]
-})
+    '@nuxt/test-utils',
+  ],
+});
